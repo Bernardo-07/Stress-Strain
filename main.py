8868121e0e9e21a2d397d0aea2aa079ce96a9d6c
Bernardo-07 <bernardoduarte271@gmail.com>
2023-12-08 15:32:25 -0300

    max and min function

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def pontos_criticos(x, y):
+
+    ymin, xmin, ymax, xmax = list(), list(), list(), list()
+    indices_min, indices_max = list(), list()
+    y0 = y[0]
+
+    for i in range(0, len(y)):
+
+        a = y[i] - y0
+
+        if(a > 0  and y[i] < y[i-1]):
+            y0 = y[i-1]
+            ymax.append(y[i-1])
+            xmax.append(x[i-1])
+            indices_max.append(i-1)
+
+        if(a < 0  and y[i] > y[i-1]):
+            y0 = y[i-1]
+            ymin.append(y[i-1])
+            xmin.append(x[i-1])
+            indices_min.append(i-1)
+
+    ymin.append(y[len(y)-1])
+    xmin.append(x[len(y)-1])  
+    indices_min.append(len(y)-1)
+
+    return ymin, xmin, ymax, xmax, indices_min, indices_max
+
 dados = np.loadtxt("dados.txt", delimiter='\t')
 
-load = dados[:,1]
-depth = dados[:,2]
+y = dados[:,1]
+x = dados[:,2]
 
-ymin, xmin, ymax, xmax = list(), list(), list(), list()
-f0 = load[0]
+Fmin, hmin, Fmax, hmax, i_min, i_max = pontos_criticos(x, y)
 
-for i in range(0, len(load)):
-
-    f = load[i]
-    a = f - f0
-    if(a > 0  and load[i] < load[i-1]):
-        ymax.append(load[i-1])
-        xmax.append(depth[i-1])
-        f0 = load[i-1]
-    if(a < 0  and load[i] > load[i-1]):
-        ymin.append(load[i-1])
-        xmin.append(depth[i-1])
-        f0 = load[i-1]
-ymin.append(load[len(load)-1])
-xmin.append(depth[len(load)-1])  
-
-plt.plot(depth, load)
+plt.plot(x, y)
 plt.title('Gráfico de Força por Profundidade')
-plt.scatter(xmin, ymin, color="r", marker="D")
-plt.scatter(xmax, ymax, color="g", marker="D")
+plt.scatter(hmin, Fmin, color="r", marker="D")
+plt.scatter(hmax, Fmax, color="g", marker="D")
 plt.xlabel('h (µm)')
 plt.ylabel('F (N)')
 plt.grid(True)
