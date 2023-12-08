e111618e334d6209e5d9927836f86849d9d7e937
Bernardo-07 <bernardoduarte271@gmail.com>
2023-12-08 11:17:44 -0300

    Load by depth plot

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+dados = np.loadtxt("dados.txt", delimiter='\t')
+
+load = dados[:,1]
+depth = dados[:,2]
+
+ymin, xmin, ymax, xmax = list(), list(), list(), list()
+f0 = load[0]
+
+for i in range(0, len(load)):
+
+    f = load[i]
+    a = f - f0
+    if(a > 0  and load[i] < load[i-1]):
+        ymax.append(load[i-1])
+        xmax.append(depth[i-1])
+        f0 = load[i-1]
+    if(a < 0  and load[i] > load[i-1]):
+        ymin.append(load[i-1])
+        xmin.append(depth[i-1])
+        f0 = load[i-1]
+ymin.append(load[len(load)-1])
+xmin.append(depth[len(load)-1])  
+
+plt.plot(depth, load)
+plt.title('Gráfico de Força por Profundidade')
+plt.scatter(xmin, ymin, color="r", marker="D")
+plt.scatter(xmax, ymax, color="g", marker="D")
+plt.xlabel('h (µm)')
+plt.ylabel('F (N)')
+plt.grid(True)
+plt.show()
