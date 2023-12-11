91a9c60d3c3b51bfea91fd2df7a9e66a6a776cfe
Bernardo-07 <bernardoduarte271@gmail.com>
2023-12-11 10:18:27 -0300

    adjustments

--- main.py
+++ main.py
@@ -47,25 +47,25 @@ load = dados[:,1]
 depth = dados[:,2]
 
 coef_ang, coef_lin = coeficientes(depth, load)
-Fmin, hmin, Fmax, hmax, i_min, i_max = pontos_criticos(depth, load)
+newdepth = depth - (-coef_lin/coef_ang)
+Fmin, hmin, Fmax, hmax, i_min, i_max = pontos_criticos(newdepth, load)
 
 for i in range(0, len(Fmax)):
     j = i_max[i]
     k = i_min[i]
 
-    h_descarga = depth[j:k]
+    h_descarga = newdepth[j:k]
     F_descarga = load[j:k]
     F_fit = regressao_linear(h_descarga, F_descarga)
     plt.plot(h_descarga, F_fit, color='y')
 
-newdepth = depth - (-coef_lin/coef_ang)
-
-plt.plot(depth, load, color='b', label='Dados originais')
+#plt.plot(depth, load, color='b', label='Dados originais')
 plt.plot(newdepth, load, label='Curva Calibrada')
 plt.title('Gráfico de Força por Profundidade')
 plt.scatter(hmin, Fmin, color="r", marker="D")
 plt.scatter(hmax, Fmax, color="g", marker="D")
 plt.xlabel('h (µm)')
 plt.ylabel('F (N)')
+plt.legend()
 plt.grid(True)
 plt.show()
